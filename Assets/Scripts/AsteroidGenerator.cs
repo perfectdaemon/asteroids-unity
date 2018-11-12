5114344aec350138e0a41907fb41f0b8e4b7b88b
perfectdaemon <perfectdaemon@gmail.com>
2018-11-12 23:18:53 +0700

    Started to implement subdivided cube generator

--- Assets/Scripts/AsteroidGenerator.cs
+++ Assets/Scripts/AsteroidGenerator.cs
@@ -1,4 +1,5 @@
-﻿using UnityEngine;
+﻿using System.Collections.Generic;
+using UnityEngine;
 
 public class AsteroidGenerator : MonoBehaviour
 {
@@ -10,13 +11,23 @@ public class AsteroidGenerator : MonoBehaviour
     {
         meshFilter = GetComponent<MeshFilter>();
 
-        meshFilter.mesh = Generate();
+        //meshFilter.mesh = Generate();
+        //meshFilter.mesh = new Mesh();
+        //var info = GeneratePlane(
+        //    new Vector3(0, 0, 0),
+        //    new Vector3(0, 1, 0),
+        //    new Vector3(1, 0, 0),
+        //    2, 10);
+        //meshFilter.mesh.vertices = info.vertices.ToArray();
+        //meshFilter.mesh.triangles = info.triangles.ToArray();
+
+        meshFilter.mesh = GenerateCube(cubeSize, 10);
     }
 
     // Use this for initialization
     public void Start()
     {
-        
+
     }
 
     // Update is called once per frame
@@ -25,7 +36,48 @@ public class AsteroidGenerator : MonoBehaviour
 
     }
 
-    private Mesh Generate()
+    private Mesh GenerateCube(float cubeSize, int subdivisionCount)
+    {
+        var mesh = new Mesh();
+
+        var halfSize = cubeSize / 2;
+
+        var subdivSize = cubeSize / subdivisionCount;
+
+        var top = GeneratePlane(
+            new Vector3(-halfSize, halfSize, -halfSize),
+            new Vector3(0, 0, 1),
+            new Vector3(1, 0, 0),
+            subdivSize,
+            subdivisionCount,
+            0 * subdivisionCount * subdivisionCount
+        );
+
+        var bottom = GeneratePlane(
+            new Vector3(-halfSize, -halfSize, halfSize),
+            new Vector3(0, 0, -1),
+            new Vector3(1, 0, 0),
+            subdivSize,
+            subdivisionCount,
+            1 * subdivisionCount * subdivisionCount
+        );
+
+        var allVertices = new List<Vector3>();
+        allVertices.AddRange(top.vertices);
+        allVertices.AddRange(bottom.vertices);
+
+        var allTriangles = new List<int>();
+
+        allTriangles.AddRange(top.triangles);
+        allTriangles.AddRange(bottom.triangles);
+
+        mesh.vertices = allVertices.ToArray();
+        mesh.triangles = allTriangles.ToArray();
+
+        return mesh;
+    }
+
+    private Mesh GenerateSimpleCube()
     {
         var mesh = new Mesh();
 
@@ -58,11 +110,48 @@ public class AsteroidGenerator : MonoBehaviour
 
             5, 1, 2,
             2, 6, 5,
-            
-            7, 3, 0, 
+
+            7, 3, 0,
             0, 4, 7,
         };
 
         return mesh;
+    }
+
+    private VerticesTriangleInfo GeneratePlane(Vector3 start, Vector3 rowVector, Vector3 colVector, float size, int count, int startIndex)
+    {
+        rowVector.Normalize();
+        colVector.Normalize();
+
+        var result = new VerticesTriangleInfo()
+        {
+            triangles = new List<int>(),
+            vertices = new List<Vector3>()
+        };
+
+        for (int row = 0; row < count; ++row)
+            for (int col = 0; col < count; ++col)
+                result.vertices.Add(start + (colVector * size * col) + (rowVector * size * row));
+
+        for (int row = 0; row < count - 1; ++row)
+            for (int col = 0; col < count - 1; ++col)
+                result.triangles.AddRange(new int[] {
+                    startIndex + count * row + col + 1,
+                    startIndex + count * row + col,
+                    startIndex + count * (row + 1) + col,
+
+                    startIndex + count * (row + 1) + col,
+                    startIndex + count * (row + 1) + col + 1,
+                    startIndex + count * row + col + 1
+                });
+
+        return result;
+    }
+
+    internal struct VerticesTriangleInfo
+    {
+        public List<Vector3> vertices;
+
+        public List<int> triangles;
     }
 }
